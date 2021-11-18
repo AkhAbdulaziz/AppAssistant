1aaf26ffafda854c8b73a338767a8144949442f4
AkhAbdulaziz <akh.abdulazizs@gmail.com>
2021-11-18 11:16:01 +0500

    Play market release version

--- app/src/main/java/uz/gita/appassistant/MainActivity.kt
+++ app/src/main/java/uz/gita/appassistant/MainActivity.kt
@@ -5,15 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 
-//class MainActivity : AppCompatActivity(R.layout.activity_main)
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        throw IllegalArgumentException("Bu bizning execption")
 
         val navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
         val graph = navHost.navController.navInflater.inflate(R.navigation.app_nav)
